ba1c933b0658a8852f432c518a5ea4096eaccd00
Drew Gottlieb <gottlieb.drew@gmail.com>
2013-10-06 05:12:41 +0000

    Makes a backup of .vimrc_bundles and .vimrc_vundle files too.

--- setup_vimrc.sh
+++ setup_vimrc.sh
@@ -5,9 +5,15 @@ OLDDIR=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-# Backup old vimrc file
+# Backup old vimrc files
 if [ -f ~/.vimrc ]; then
 	mv ~/.vimrc ~/.vimrc.old
+fi;
+if [ -f ~/.vimrc_bundles ]; then
+	mv ~/.vimrc_bundles ~/.vimrc_bundles.old
+fi;
+if [ -f ~/.vimrc_vundle ]; then
+	mv ~/.vimrc_vundle ~/.vimrc_vundle.old
 fi;
 
 # Create hardlinks between local vimrc files and the actual vimrc files
